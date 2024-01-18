8f365af258b05c9d8c2813840aba31f0aedff832
Suyesh Shankar <suyesh@gmail.com>
2024-01-18 11:19:47 +0530

    cors headers added

--- app.js
+++ app.js
@@ -8,7 +8,16 @@ const app = express();
 const port = 3000;
 
 // Enable CORS
-app.use(cors());
+//app.use(cors());
+
+// In your server-side code
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 
 // Define routes
 
